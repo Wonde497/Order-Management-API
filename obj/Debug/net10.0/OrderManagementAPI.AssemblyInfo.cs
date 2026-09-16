0b348d2e89d332105f4cafe7366b4dbbdae20dab
Wonde497 <wonde.c2024@gmail.com>
2026-09-16 09:43:43 +0300

    injects connection string

--- obj/Debug/net10.0/OrderManagementAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/OrderManagementAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40c08db64db43b0850fadde26b6af7f99c853a59")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
